068760c35c1cbad42d513347235e7531f7d59ba7
afontana405 <alex13579@icloud.com>
2023-10-31 16:08:24 -0500

    next playable location feature framework layed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var playerOnesTurn = true;
 var lastSpotPlayed; 
+var nextPlayableLocation;
 
 document.addEventListener("click", function() {
     if (event.target.tagName === 'DIV') {
@@ -11,28 +12,38 @@ document.addEventListener("click", function() {
 document.getElementById('submit-btn').addEventListener("click", submitTurn);
 
 function playersTurn() {
-    if (lastSpotPlayed) {
+    if (lastSpotPlayed) { // makes sure player can only make one move per turn
         lastSpotPlayed.innerHTML = '';
     }
-    if (playerOnesTurn && event.target.textContent === '') {
+    if (playerOnesTurn && event.target.textContent === '') { // if player ones turn, place an X where they click
         lastSpotPlayed = event.target;
         event.target.textContent = 'X';
-    } else if (!playerOnesTurn && event.target.textContent === '') {
+    } else if (!playerOnesTurn && event.target.textContent === '') { // if player twos turn, place an O where they click
         lastSpotPlayed = event.target;
         event.target.textContent = 'O';
+    }
+}
+
+function submitTurn() {
+    nextSquare(); 
+    lastSpotPlayed = ''; // clears the variable
+    if (playerOnesTurn) {
+        playerOnesTurn = false; // makes it player 2s turn
+    } else if (!playerOnesTurn) {
+        playerOnesTurn = true; // makes it player 1s turn
     }
     checkBingo();
 }
 
-function submitTurn() {
-    lastSpotPlayed = '';
-    if (playerOnesTurn) {
-        playerOnesTurn = false;
-    } else if (!playerOnesTurn) {
-        playerOnesTurn = true;
+function nextSquare() {
+    if (nextPlayableLocation) {
+        document.getElementById(nextPlayableLocation).classList.remove('playable');
     }
+    nextPlayableLocation = lastSpotPlayed.className;
+    console.log(nextPlayableLocation);
+    document.getElementById(nextPlayableLocation).classList.add('playable');
 }
 
 function checkBingo() {
-console.log(event.target.className)
+
 }
