4a21c7b58f66486416bb399b2c7de0f7985749fa
afontana405 <alex13579@icloud.com>
2023-10-31 13:22:25 -0500

    alternates between x turn and o turn

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var playerOnesTurn = true;
+
+document.addEventListener("click", function() {
+    if (event.target.tagName === 'DIV') {
+        playersTurn();
+    }
+});
+
+function playersTurn() {
+    if (playerOnesTurn) {
+        event.target.textContent = 'X';
+        playerOnesTurn = false;
+    } else if (!playerOnesTurn) {
+        event.target.textContent = 'O';
+        playerOnesTurn = true;
+    }
+}
