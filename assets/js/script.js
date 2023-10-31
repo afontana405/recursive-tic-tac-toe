9eabf75d083e9a77867dc3242d1ff04cfa2054cc
afontana405 <alex13579@icloud.com>
2023-10-31 14:45:59 -0500

    turn is not logged until submit turn pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,38 @@
 var playerOnesTurn = true;
+var lastSpotPlayed; 
 
 document.addEventListener("click", function() {
     if (event.target.tagName === 'DIV') {
+        // console.log(event.target.parentElement);
         playersTurn();
     }
 });
 
+document.getElementById('submit-btn').addEventListener("click", submitTurn);
+
 function playersTurn() {
+    if (lastSpotPlayed) {
+        lastSpotPlayed.innerHTML = '';
+    }
     if (playerOnesTurn && event.target.textContent === '') {
+        lastSpotPlayed = event.target;
         event.target.textContent = 'X';
-        playerOnesTurn = false;
     } else if (!playerOnesTurn && event.target.textContent === '') {
+        lastSpotPlayed = event.target;
         event.target.textContent = 'O';
-        playerOnesTurn = true;
     }
     checkBingo();
 }
 
-function checkBingo() {
+function submitTurn() {
+    lastSpotPlayed = '';
+    if (playerOnesTurn) {
+        playerOnesTurn = false;
+    } else if (!playerOnesTurn) {
+        playerOnesTurn = true;
+    }
+}
 
+function checkBingo() {
+console.log(event.target.className)
 }
