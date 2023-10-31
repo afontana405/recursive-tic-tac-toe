edc6b3b8061cc33f8bed09747a719323a52592ae
afontana405 <alex13579@icloud.com>
2023-10-31 17:08:38 -0500

    all squares playable on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,23 @@ var playerOnesTurn = true;
 var lastSpotPlayed; 
 var nextPlayableLocation;
 
+window.addEventListener('load', gameStart);
+
 document.addEventListener("click", function() {
-    if (event.target.tagName === 'DIV') {
+    if (event.target.tagName === 'DIV' && event.target.parentElement.parentElement.classList.contains('playable')) {
         // console.log(event.target.parentElement);
         playersTurn();
     }
 });
 
 document.getElementById('submit-btn').addEventListener("click", submitTurn);
+
+function gameStart() {
+    var gameBoard = document.getElementsByClassName('box');
+    for (var i = 0; i < gameBoard.length; i++) {
+        gameBoard[i].classList.add('playable');
+    }
+}
 
 function playersTurn() {
     if (lastSpotPlayed) { // makes sure player can only make one move per turn
