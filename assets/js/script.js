11ebf7c5319a2fc3f3803372d09f915c89ed6aa3
afontana405 <alex13579@icloud.com>
2023-11-01 01:14:05 -0500

    checks for bingo now

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,20 +36,20 @@ function playersTurn() {
 function submitTurn() {
     if (lastSpotPlayed != '') {
         nextSquare(); 
+        checkBingo();
         lastSpotPlayed = ''; // clears the variable
         if (playerOnesTurn) {
             playerOnesTurn = false; // makes it player 2s turn
         } else if (!playerOnesTurn) {
             playerOnesTurn = true; // makes it player 1s turn
         }
-        checkBingo();
     }
 }
 
 function nextSquare() {
-    if (nextPlayableLocation) {
+    if (nextPlayableLocation) { // removes class of previous playable location
         document.getElementById(nextPlayableLocation).classList.remove('playable');
-    } else {
+    } else { // runs only after first move, clears class from every location
         for (var i = 0; i < gameBoard.length; i++) {
             gameBoard[i].classList.remove('playable');
         }
@@ -59,6 +59,26 @@ function nextSquare() {
     document.getElementById(nextPlayableLocation).classList.add('playable');
 }
 
+// my inefficient way to check for 3 X's or O's in a row
 function checkBingo() {
-
+    var containerEl = lastSpotPlayed.parentElement;
+    var tiles = containerEl.children;
+    var [tL, tM, tR, mL, mM, mR, bL, bM, bR] = tiles;
+    if (tL.textContent != '' && tL.textContent === tM.textContent && tL.textContent ===  tR.textContent) {
+        containerEl.innerHTML = 'X';
+    } else if (mL.textContent != '' && mL.textContent === mM.textContent && mL.textContent ===  mR.textContent) {
+        containerEl.innerHTML = 'X';
+    } else if (bL.textContent != '' && bL.textContent === bM.textContent && bL.textContent ===  bR.textContent) {
+        containerEl.innerHTML = 'X';
+    } else if (tL.textContent != '' && tL.textContent === mL.textContent && tL.textContent === bL.textContent) {
+        containerEl.innerHTML = 'X';
+    } else if (tM.textContent != '' && tM.textContent === mM.textContent && tM.textContent === bM.textContent) {
+        containerEl.innerHTML = 'X';
+    } else if (tR.textContent != '' && tR.textContent === mR.textContent && tR.textContent === bR.textContent) {
+        containerEl.innerHTML = 'X';
+    } else if (tL.textContent != '' && tL.textContent === mM.textContent && tL.textContent === bR.textContent) {
+        containerEl.innerHTML = 'X'; 
+    } else if (tR.textContent != '' && tR.textContent === mM.textContent && tR.textContent === bL.textContent) {
+        containerEl.innerHTML = 'X';
+    }
 }
