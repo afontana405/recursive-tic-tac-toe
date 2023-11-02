154bb49a1f26e1d6f1e0201770a37acaf885f957
afontana405 <alex13579@icloud.com>
2023-11-02 00:05:17 -0500

    will not add playable class to completed squares

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,8 @@
 var playerOnesTurn = true;
 var lastSpotPlayed; 
 var nextPlayableLocation;
+var allContainers = document.getElementsByClassName("container");
 var gameBoard = document.getElementsByClassName('box');
-
 window.addEventListener('load', gameStart);
 
 document.addEventListener("click", function() {
@@ -35,8 +35,8 @@ function playersTurn() {
 
 function submitTurn() {
     if (lastSpotPlayed != '') {
-        nextSquare(); 
         checkBingo();
+        nextSquare(); 
         lastSpotPlayed = ''; // clears the variable
         if (playerOnesTurn) {
             playerOnesTurn = false; // makes it player 2s turn
@@ -47,16 +47,20 @@ function submitTurn() {
 }
 
 function nextSquare() {
-    if (nextPlayableLocation) { // removes class of previous playable location
-        document.getElementById(nextPlayableLocation).classList.remove('playable');
-    } else { // runs only after first move, clears class from every location
-        for (var i = 0; i < gameBoard.length; i++) {
-            gameBoard[i].classList.remove('playable');
+    for (var i = 0; i < gameBoard.length; i++) {
+        gameBoard[i].classList.remove('playable');
+    }
+    nextPlayableLocation = document.getElementById(lastSpotPlayed.className);
+    let childEl = nextPlayableLocation.children[0];
+    if (childEl.textContent != 'X' && childEl.textContent != 'O') {
+        nextPlayableLocation.classList.add('playable');
+    } else {
+        for (let i = 0; i < gameBoard.length; i++) {
+            if (allContainers[i].textContent != 'X' && allContainers[i].textContent != 'O') {
+                gameBoard[i].classList.add('playable');
+            }
         }
     }
-    nextPlayableLocation = lastSpotPlayed.className;
-    // console.log(nextPlayableLocation);
-    document.getElementById(nextPlayableLocation).classList.add('playable');
 }
 
 // my inefficient way to check for 3 X's or O's in a row
@@ -65,20 +69,64 @@ function checkBingo() {
     var tiles = containerEl.children;
     var [tL, tM, tR, mL, mM, mR, bL, bM, bR] = tiles;
     if (tL.textContent != '' && tL.textContent === tM.textContent && tL.textContent ===  tR.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (mL.textContent != '' && mL.textContent === mM.textContent && mL.textContent ===  mR.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (bL.textContent != '' && bL.textContent === bM.textContent && bL.textContent ===  bR.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (tL.textContent != '' && tL.textContent === mL.textContent && tL.textContent === bL.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (tM.textContent != '' && tM.textContent === mM.textContent && tM.textContent === bM.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (tR.textContent != '' && tR.textContent === mR.textContent && tR.textContent === bR.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (tL.textContent != '' && tL.textContent === mM.textContent && tL.textContent === bR.textContent) {
-        containerEl.innerHTML = 'X'; 
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     } else if (tR.textContent != '' && tR.textContent === mM.textContent && tR.textContent === bL.textContent) {
-        containerEl.innerHTML = 'X';
+        if (playerOnesTurn) {
+            containerEl.textContent = 'X'
+        } else {
+            containerEl.textContent = 'O'
+        }
+        checkMegaBingo();
     }
+}
+
+function checkMegaBingo() {
+
 }
